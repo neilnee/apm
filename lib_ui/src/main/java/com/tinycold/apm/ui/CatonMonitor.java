d3332ca0505d71f1424d960d17ed1eea109b63da
neil <neilni324@gmail.com>
2019-06-28 17:24:43 +0800

    提取卡顿监控中的独立卡顿概况信息

--- lib_ui/src/main/java/com/tinycold/apm/ui/CatonMonitor.java
+++ lib_ui/src/main/java/com/tinycold/apm/ui/CatonMonitor.java
@@ -5,6 +5,8 @@ import android.os.HandlerThread;
 import android.os.Looper;
 import android.util.Printer;
 
+import androidx.annotation.NonNull;
+
 public class CatonMonitor {
     /**
      * 使用Looper中的log日志监控方案
@@ -15,7 +17,8 @@ public class CatonMonitor {
      */
     public static final int MONITOR_MSG = 2;
 
-    private static final CheckerV1 mCheckerV1 = new CheckerV1();
+    private static final CatonSummary mSummary = new CatonSummary();
+    private static Checker mChecker = null;
     private static int mWay = 0;
 
     public static void start(int way) {
@@ -28,6 +31,9 @@ public class CatonMonitor {
         mWay = way;
         switch (mWay) {
             case MONITOR_LOG: {
+                if (!(mChecker instanceof CheckerV1)) {
+                    mChecker = new CheckerV1(mSummary);
+                }
                 Looper.getMainLooper().setMessageLogging(new Printer() {
                     private final String START = ">>>>> Dispatching to";
                     private final String FINISH = "<<<<< Finished to";
@@ -35,9 +41,9 @@ public class CatonMonitor {
                     @Override
                     public void println(String s) {
                         if (s.startsWith(START)) {
-                            mCheckerV1.start();
+                            mChecker.start();
                         } else if (s.startsWith(FINISH)) {
-                            mCheckerV1.end();
+                            mChecker.end();
                         }
                     }
 
@@ -45,7 +51,10 @@ public class CatonMonitor {
                 break;
             }
             case MONITOR_MSG: {
-
+                if (!(mChecker instanceof CheckerV2)) {
+                    mChecker = new CheckerV2(mSummary);
+                }
+                break;
             }
         }
     }
@@ -60,28 +69,43 @@ public class CatonMonitor {
                 break;
             }
         }
+        mChecker = null;
     }
 
     public static CatonSummary summary() {
-        return mCheckerV1.mDetail;
+        switch (mWay) {
+            case MONITOR_LOG: {
+                break;
+            }
+        }
+        return mChecker.mSummary;
     }
 
-    private static class CheckerV1 {
+    private static abstract class Checker {
+        protected final CatonSummary mSummary;
+        protected Checker(@NonNull CatonSummary summary) {
+            mSummary = summary;
+        }
+        protected abstract void start();
+        protected abstract void end();
+    }
+
+    private static class CheckerV1 extends Checker {
         private final int CATON_INTERVAL = 600;
-        private final CatonSummary mDetail = new CatonSummary();
         private final HandlerThread mHandlerThread = new HandlerThread("CheckerV1");
         private final Handler mHandler;
         private final Runnable mRun = new Runnable() {
             @Override
             public void run() {
                 // 执行卡顿时的统计
-                mDetail.mCatonTimes++;
+                mSummary.mCatonTimes++;
             }
         };
 
         private long mCheckStart = 0;
 
-        public CheckerV1() {
+        public CheckerV1(@NonNull CatonSummary summary) {
+            super(summary);
             mHandlerThread.start();
             mHandler = new Handler(mHandlerThread.getLooper());
         }
@@ -95,15 +119,28 @@ public class CatonMonitor {
             if (mHandler.hasCallbacks(mRun)) {
                 mHandler.removeCallbacks(mRun);
             } else if (mCheckStart > 0) {
-                mDetail.mCatonDuration += System.currentTimeMillis() - mCheckStart;
+                mSummary.mCatonDuration += System.currentTimeMillis() - mCheckStart;
             }
             mCheckStart = 0;
         }
 
     }
 
-    private static class CheckerV2 {
-        
+    private static class CheckerV2 extends Checker {
+
+        protected CheckerV2(@NonNull CatonSummary summary) {
+            super(summary);
+        }
+
+        @Override
+        protected void start() {
+
+        }
+
+        @Override
+        protected void end() {
+
+        }
     }
 
 }
